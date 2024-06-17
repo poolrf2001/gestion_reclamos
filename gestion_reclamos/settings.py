2cb6365ac5688c69b0bf8cf3993b3fa2e1ec8b91
PoolRF <pwrf2001@gmail.com>
2024-06-16 21:11:04 -0500

    cambio pymysql

--- gestion_reclamos/settings.py
+++ gestion_reclamos/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from dotenv import load_dotenv
 load_dotenv()
 
